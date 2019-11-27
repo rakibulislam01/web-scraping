b0b1336c28806da93e0c62d190091f2ee70d4e16
Rakibul <mrakibul70@gmail.com>
2019-11-27 13:12:18 +0600

    11 - Web Scraping Quotes, Authors and Title

--- TheSpider/TheSpider/spiders/quotes_spider.py
+++ TheSpider/TheSpider/spiders/quotes_spider.py
@@ -8,5 +8,14 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield {'titletext': title}
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+            yield {
+                'title': title,
+                'author': author,
+                'tag': tag
+            }
