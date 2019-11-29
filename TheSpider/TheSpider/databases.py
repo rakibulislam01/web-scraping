ba44ae14b619036ecf40fccda993e455ef08cc16
rakibulislam01 <mrakibul70@gmail.com>
2019-11-30 02:44:41 +0600

    Crawle data and save into mysql database

--- TheSpider/TheSpider/databases.py
+++ TheSpider/TheSpider/databases.py
@@ -1,15 +1,19 @@
-import sqlite3
+"""
+It create for test database.
+"""
 
-conn = sqlite3.connect('myquoutes.db')
-curr = conn.cursor()
-
+# import sqlite3
+#
+# conn = sqlite3.connect('myquoutes.db')
+# curr = conn.cursor()
+#
 # curr.execute("""create table quotes_tb(
 #                 title text,
 #                 author text,
 #                 tag text
 #                 )""")
-
-curr.execute("""insert into quotes_tb values ('Python is Awesome!', 'Python', 'Programming')""")
-
-conn.commit()
-conn.close()
+#
+# curr.execute("""insert into quotes_tb values ('Python is Awesome!', 'Python', 'Programming')""")
+#
+# conn.commit()
+# conn.close()
--- TheSpider/TheSpider/pipelines.py
+++ TheSpider/TheSpider/pipelines.py
@@ -5,6 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
+
 
 class ThespiderPipeline(object):
     def process_item(self, item, spider):
@@ -12,6 +14,31 @@ class ThespiderPipeline(object):
 
 
 class QuotesPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect('myquoutes.db')
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS myquoutes""")
+        self.curr.execute("""create table quotes_tb(
+                        title text,
+                        author text,
+                        tag text
+                        )""")
+
     def process_item(self, item, spider):
-        print("Pipeline :" + item['title'][0])
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.curr.execute("""insert into quotes_tb values (?, ?, ?)""", (
+            item['title'][0],
+            item['author'][0],
+            item['tag'][0]
+        ))
+        self.conn.commit()
