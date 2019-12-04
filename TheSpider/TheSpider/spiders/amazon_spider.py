fc438ec8dfc6f456e03cc83cdb367963d6ca7323
rakibulislam01 <mrakibul70@gmail.com>
2019-12-05 00:50:05 +0600

    Scrape data from amazon website and use user agent

--- TheSpider/TheSpider/spiders/amazon_spider.py
+++ TheSpider/TheSpider/spiders/amazon_spider.py
@@ -12,13 +12,13 @@ class QuotesSpider(scrapy.Spider):
         items = AmazonItem()
 
         product_name = response.css('.a-color-base.a-text-normal').css('::text').extract()
-        product_author = response.css('.a-color-secondary .a-size-base.a-link-normal').css('::text').extract()
-        product_price = response.css('.a-spacing-top-small .a-price-fraction , .a-spacing-top-small .a-price-whole').css('::text').extract()
+        product_author = response.css('.a-color-secondary .a-size-base+ .a-size-base').css('::text').extract()
+        product_price = response.css('.a-spacing-top-small .a-price-whole').css('::text').extract()
         product_imagelink = response.css('.s-image::attr(src)').extract()
 
         items['product_name'] = product_name
-        items['product_author'] = 'product_author'  # get
-        items['product_price'] = 'product_price'  # get
-        items['product_imagelink'] = 'product_imagelink'
+        items['product_author'] = product_author  # get
+        items['product_price'] = product_price  # get
+        items['product_imagelink'] = product_imagelink
 
         yield items
